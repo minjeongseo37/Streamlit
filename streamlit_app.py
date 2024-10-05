97b745a90a8812c7dd87b189a9acf067366cd3ba
minjeongseo37 <minjeongseo37@gmail.com>
2024-10-05 18:24:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt # 시각화 라이브러리
+import matplotlib.pyplot as plt  # 시각화 라이브러리
 import plotly.express as px
+import plotly.graph_objects as go
 
-
+# Title
 st.title('■ Visualization_241005 ■')
 
 st.write("")
 st.write("")
 
+# Subheader with custom margin
 st.markdown(
     """
     <div style="margin-left: 50px; margin-right: 0px;">
@@ -19,84 +21,25 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# st.subheader('1. Streamlit for diverse forms of graph visualizations')
+# Pie chart
+labels = ['A', 'B', 'C', 'D']
+values = [300, 200, 100, 500]
+fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3)])
 
-import plotly.graph_objects as go
-labels = ['A','B','C','D']
-values = [300,200,100,500]
-fig1 = go.Figure(data = [go.Pie(labels = labels, values = values, hole =.3)])
-
-import plotly.express as px
+# Bar chart
 data_canada = px.data.gapminder().query("country == 'Canada'")
-fig2 = px.bar(data_canada, x='year', y='pop', color = 'pop')
+fig2 = px.bar(data_canada, x='year', y='pop', color='pop')
 
 # Create two columns with 3:1 ratio
-col1, col2 = st.columns([3, 1])  # col1은 col2의 절반 너비 열 생성
+col1, col2 = st.columns([3, 1])
 
-# First column with pie chart
+# First column with bar chart
 with col1:
-    st.plotly_chart(fig2, use_container_width=True)  # 바 차트 출력  
+    st.plotly_chart(fig2, use_container_width=True)
 
-# Second column with bar chart and image
+# Second column with pie chart
 with col2:
-    st.plotly_chart(fig1, use_container_width=True)  # 파이 차트 출력
+    st.plotly_chart(fig1, use_container_width=True)
 
-st.markdown("</div>", unsafe_allow_html=True)  # div 닫기
-
-
-
-df = px.data.gapminder()
-fig3 = px.scatter(df.query("year == 2007"), x = 'gdpPercap' , y = 'lifeExp', size = 'pop', color = 'continent')
-st.plotly_chart(fig3)
-
-
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-
-st.markdown(
-    """
-    <div style="margin-left: 50px; margin-right: 0px;">
-        <h3>2. Streamlit for sin and cos function visualization</h3>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-#st.subheader('2. Streamlit for sin and cos function visualization')
-st.write("")
-st.write("")
-x_start = st.slider('x 시작값' ,  0.0, 10.0, 0.0)
-x_end = st.slider('x 시작값' ,  10.0, 20.0, 10.0)
-
-
-x = np.linspace(x_start, x_end)
-
-y_sin = np.sin(x)
-y_cos = np.cos(x)
-
-
-fig , ax = plt.subplots()
-
-ax.plot(x, y_sin)
-ax.plot(x, y_cos)
-ax.legend(['sin', 'cos'])
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-
-ax.set_title('sin and cos fuction')
-
-st.pyplot(fig)
-
-@st.cache
-def expensive_computataion(x):
-    return np.sin(x) + np.cos(x)
-
-result = expensive_computataion(x)
-
-
-st.markdown("</div>", unsafe_allow_html=True)  # div 닫기
-
+# Scatter plot
+df = px
