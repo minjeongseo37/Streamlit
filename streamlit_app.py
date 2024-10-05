6c99e764753c6de62db7ca16bdad640adfc72a50
minjeongseo37 <minjeongseo37@gmail.com>
2024-10-05 17:33:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,10 @@ data_canada
 fig = px.bar(data_canada, x='year', y='pop')
 st.plotly_chart(fig)
 
+import plotly.graph_objects as go
 labels = ['A','B','C','D']
 values = [300,200,100,500]
 fig = go.Figure(data = [go.Pie(labels = labels, values = values, hole =.3)])
 st.plotly_pie(fig)
+
 
