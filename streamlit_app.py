4e8928ec18edeaf8a59a92f83e84fd654a768cfb
minjeongseo37 <minjeongseo37@gmail.com>
2024-10-05 17:39:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.plotly_chart(fig)
 
 import plotly.express as px
 data_canada = px.data.gapminder().query("country == 'Canada'")
-fig = px.bar(data_canada, x='year', y='pop')
+fig = px.bar(data_canada, x='year', y='pop', color = 'pop'))
 st.plotly_chart(fig)
 
 
